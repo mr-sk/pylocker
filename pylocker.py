512a37277ac5b5c9e8358ab43302aa6ee14fad64
mr-sk <bsgro@cavalier.fios-router.home>
2019-09-02 17:05:17 -0400

    Additional fixes

--- pylocker.py
+++ pylocker.py
@@ -2,8 +2,9 @@ import argparse
 import json
 import os
 import re
-import stdiomask
 import secrets
+import stdiomask
+import sys
 
 from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
@@ -13,6 +14,10 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 backend = default_backend()
+
+def signal_handler(sig, frame):
+    print("[!] Signal caught, exiting.")
+    sys.exit(0)
 
 def _derive_key(password, salt, iterations):
     """Derive a secret key from a given password and salt"""
@@ -43,117 +48,142 @@ def password_decrypt(token, password):
     except:
         return False
 
-     
-def get_lockerfile():
-    filename = input("Enter locker file path: ")
-    if os.path.exists(filename):
-        print("File exists, try again")
-        get_lockerfile()
-
-    return filename
-
 def get_passphrase():
     passphrase = stdiomask.getpass(prompt='Enter passphrase: ', mask='*')
 
     return passphrase
+    
+def set_passphrase():
+    passphrase = stdiomask.getpass(prompt='Enter passphrase: ', mask='*')
+    confirmed_passphrase = stdiomask.getpass(prompt='Confirm passphrase: ', mask='*')    
+
+    if passphrase != confirmed_passphrase:
+        print("[!] Passphrases do not match")
+        get_passphrase()
+    
+    return passphrase
 
 def main_menu(filename):
-    print("[!] Current locker file {}".format(filename))
-    cmd_input = input("[a]dd entry, [s]ave & exit or search:")
+    print("Current locker file '{}'".format(os.path.abspath(filename)))
+    cmd_input = input("[a]dd entry, [s]how-all, [e]xit or search: ")
+    cmd_input = cmd_input.lower().strip()
 
     return cmd_input
 
+def add_entry(decrypted_locker_dict, passphrase, filename):
+    entry_dict = {}
+    meta = input("Locker Name:  ")
+    email = input("Email Address: ")
+    password = input("Password: ")
+
+    extra_dict = {}
+    extra = True
+    extra_count = 1
+    while extra != '':
+        key = "Extra {}: ".format(extra_count)
+        extra = input(key)
+        extra_dict[key] = extra
+        extra_count += 1
+        
+    # Dict updates after final empty entry has been added, remove. 
+    del extra_dict[key]
+    print("Extra dict: {}".format(extra_dict))
+
+    entry_dict = {
+        meta : {
+            "email" : email,
+            "password" : password,
+            "extra": extra_dict
+        }
+    }
+
+    if bool(decrypted_locker_dict) is False:
+        decrypted_locker_dict = entry_dict
+    else:
+        decrypted_locker_dict.update(entry_dict)
+        decrypted_locker_dict = dict(decrypted_locker_dict, **entry_dict)
+
+    return decrypted_locker_dict
+
+def write_file(decrypted_locker_dict, passphrase, filename):
+    # Encrypt
+    locker_bytes = json.dumps(decrypted_locker_dict).encode()
+    encrypted_locker = password_encrypt(locker_bytes, passphrase, 10)
+
+    # Write the encrypted bytes to the output file
+    with open(filename, 'wb') as f:
+        f.write(encrypted_locker)
+        print("Wrote {} encrypted {} to {}".format(len(encrypted_locker), type(encrypted_locker), filename))
+
+    json_string = decrypted_locker_dict
+    print("json string: {} with type {}".format(json_string, type(json_string)))
+    with open(filename + '.json-debug', 'w', encoding='utf-8') as f:
+        json.dump(json_string, f, ensure_ascii=False, indent=4)
+
+        
 def main():
     cmd_parser = argparse.ArgumentParser(description='Decrypt locker')
-    cmd_parser.add_argument('-f', '--file', help='File location to descrypt', required=False)
+    cmd_parser.add_argument('-f', '--file', help='File location to descrypt', required=True)
     args = vars(cmd_parser.parse_args())
     filename = args['file']
     locker = {}
+    decrypted_locker_dict = {}
     
     # Check if file exists and is not empty
     if os.path.exists(filename) and os.stat(filename).st_size != 0:
-        print("Locker file found, or file is empty")
+        print("Locker file found")
         passphrase = get_passphrase()
-        #key = _derive_key(passphrase, 8, 10)
+
         # Read in contents of encrypted file
         with open(filename) as f:
             encrypted_bytes = f.read().encode()
 
-        print("Encrypted bytes {}".format(encrypted_bytes))
-
         # Decrypt
         decrypted_locker = password_decrypt(encrypted_bytes, passphrase)
         if decrypted_locker is False:
-            print("[!] Invalid token, exiting.")
-            return False
-        
-        print("Decrypted locker {}".format(decrypted_locker))
-    else:
-        print("Unable to locate locker file")
-        filename = get_lockerfile()
-        passphrase = get_passphrase()
+            print("Invalid token, exiting.")
+            sys.exit()
 
-    cmd_input = main_menu(filename).lower().strip()
-
-    if cmd_input == 'a':
-        entry_dict = {}
-        meta = input("Meta:  ")
-        email = input("Email address: ")
-        password = input("Password: ")
-
-        extra_dict = {}
-        extra = True
-        extra_count = 1
-        while extra != '':
-            key = "Extra {}".format(extra_count)
-            extra = input(key)
-            extra_dict[key] = extra
-            extra_count += 1
-
-        # Dict updates after final empty entry has been added, remove. 
-        del extra_dict[key]
-        print(extra_dict)
-
-        entry_dict = {
-            'meta' : meta,
-            'email' : email,
-            'password' : password,
-            'extra': extra_dict
-        }
-
-        print(entry_dict)
-
-        # Encrypt
-        print(passphrase)
-        print(type(passphrase))
-        locker_bytes = json.dumps(entry_dict).encode()
-        encrypted_locker = password_encrypt(locker_bytes, passphrase, 10)
-        #print(locker_str)
-        #print(type(locker_str))
-        print(encrypted_locker)
-        # Write the encrypted bytes to the output file
-        with open(filename, 'wb') as f:
-            f.write(encrypted_locker)
-            print("[!] Wrote {} encrypted {} to {}".format(len(encrypted_locker), type(encrypted_locker), filename))
-            
-        with open(filename + '.json-debug', 'w', encoding='utf-8') as f:
-            json.dump(entry_dict, f, ensure_ascii=False, indent=4)
-
-    elif cmd_input == 's':
-        pass
-    else:
         decrypted_locker_dict = json.loads(decrypted_locker.decode("utf-8"))
-        #print(type(decrypted_locker))
-        #print(decrypted_locker_dict)
-        match = False
-        for key, value in decrypted_locker_dict.items():
-            #print("key: %s value: %s" % (key, value))
-            if key in 'meta': # regex not needed, I think. 
-                if cmd_input in value.lower().strip():
-                    print("found match")
+    else:
+        #print("Unable to locate locker file")
+        #filename = get_lockerfile()
+        print("Creating lock file at {}".format(os.path.abspath(filename)))
+        passphrase = set_passphrase()
+
+    cmd_input = False
+    while cmd_input != 'e':
+        cmd_input = main_menu(filename)
+
+        if cmd_input == 'a':
+            decrypted_locker_dict = add_entry(decrypted_locker_dict, passphrase, filename)
+
+            # Now write to file, kind of like auto-save.
+            write_file(decrypted_locker_dict, passphrase, filename)
+
+        elif cmd_input == 's':
+            if len(decrypted_locker_dict) == 0:
+                print("No items in locker")
+            else:
+                for meta_key, meta_dict in decrypted_locker_dict.items():
+                    print("Meta key: {}".format(meta_key))
+
+        elif cmd_input == 'e':
+            print("Shutting down, good-bye!")
+            sys.exit()
+
+        else:
+            match = False
+            for meta_key, meta_dict in decrypted_locker_dict.items():
+                if cmd_input in meta_key:
                     match = True
-            if match:
-                print("{}: {}".format(key, value))
+
+                if match:
+                    print("Matched on '{}'".format(meta_key))
+                    for k,v in meta_dict.items():
+                        print("\t {}: {}".format(k, v))
+
+                match = False
 
 #
 # Main
